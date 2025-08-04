c521d1fa2987b5e18cd6116debbd9ea25ba20d6b
Mikhail Karasev <seplaz38@yandex.ru>
2025-08-04 17:58:18 +0800

    add: ArticleParamsForm open/close functionality

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -3,12 +3,27 @@ import { Button } from 'src/ui/button';
 
 import styles from './ArticleParamsForm.module.scss';
 
+import { useState } from 'react';
+import clsx from 'clsx';
+import { Text } from 'src/ui/text';
+
 export const ArticleParamsForm = () => {
+	const [isOpen, setOpen] = useState<boolean>(true);
+
+	const toggleOpen = () => {
+		setOpen(!isOpen);
+	};
+
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
+			<ArrowButton isOpen={isOpen} onClick={toggleOpen} />
+			<aside
+				className={clsx(styles.container, { [styles.container_open]: isOpen })}>
 				<form className={styles.form}>
+					<Text as='h2' size={31} weight={800} uppercase>
+						Задайте параметры
+					</Text>
+
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
 						<Button title='Применить' htmlType='submit' type='apply' />
